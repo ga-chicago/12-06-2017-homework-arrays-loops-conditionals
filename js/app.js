22ea7d727a356332f444b1d7d4637bb128fa688c
Anthony <anthonyjlower@gmail.com>
2017-12-06 17:42:34 -0600

    Commit 5 - Getting to Know You answered

--- js/app.js
+++ js/app.js
@@ -31,16 +31,56 @@
 // 		};
 // 	};
 
+
 	// Fizz Buzz
 
-for(let i = 1; i <= 100; i++){;
-	if (i % 3 === 0 && i % 5 === 0) {
-		console.log("FizzBuzz");
-	} else if(i % 5 === 0){
-		console.log("Buzz");
-	} else if(i % 3 === 0){
-		console.log("Fizz");
-	} else{
-		console.log(i);
-	};
-};
+// for(let i = 1; i <= 100; i++){;
+// 	if (i % 3 === 0 && i % 5 === 0) {
+// 		console.log("FizzBuzz");
+// 	} else if(i % 5 === 0){
+// 		console.log("Buzz");
+// 	} else if(i % 3 === 0){
+// 		console.log("Fizz");
+// 	} else{
+// 		console.log(i);
+// 	};
+// };
+
+
+	// Getting to know you
+
+const thom = ["Thom", 1000, "Christchurch"]
+const karolin = ["Karolin", 16, "New York"]
+const kristyn = ["Kristyn", 5, "Pittsburgh"]
+const matt = ["Matt H", 186, "Philadelphia"]
+
+thom.shift();
+thom.unshift("Gameboy");
+karolin[1] = 17;
+matt[2] = "Gotham City";
+kristyn.pop();
+kristyn.push("Brooklyn");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
