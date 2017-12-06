287c84077e58ffb9a68458154629f685df88b606
Anthony <anthonyjlower@gmail.com>
2017-12-06 17:33:52 -0600

    Commit 4 - Fizz Buzz answered

--- js/app.js
+++ js/app.js
@@ -30,3 +30,17 @@
 // 		console.log("...why does the red dot always get away...");
 // 		};
 // 	};
+
+	// Fizz Buzz
+
+for(let i = 1; i <= 100; i++){;
+	if (i % 3 === 0 && i % 5 === 0) {
+		console.log("FizzBuzz");
+	} else if(i % 5 === 0){
+		console.log("Buzz");
+	} else if(i % 3 === 0){
+		console.log("Fizz");
+	} else{
+		console.log(i);
+	};
+};
