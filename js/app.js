7cd5d4be3ab6a8e07e68cd62de41dc918e45877e
Kait Mikitin <kait.mikitin@gmail.com>
2017-12-06 19:48:58 -0600

    Commit 5 - Getting to Know You answered

--- js/app.js
+++ js/app.js
@@ -16,6 +16,7 @@
 // 	console.log("Love me, pet me! HSSSSSS!");
 // 	i++;
 // }
+
 // const catThoughts = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."]
 
 // for (i = 1; i <=20; i++) {
@@ -27,14 +28,40 @@
 // }
 
 // Fizz Buzz
-for (i = 1; i <= 100; i++) {
-	if (i % 3 === 0 && i % 5 === 0) {
-		console.log("FizzBuzz");
-	} else if (i % 3 === 0) {
-		console.log("Fizz");
-	} else if (i % 5 === 0) {
-		console.log("Buzz");
-	} else {
-		console.log(i);
-	}
-}
+// for (i = 1; i <= 100; i++) {
+// 	if (i % 3 === 0 && i % 5 === 0) {
+// 		console.log("FizzBuzz");
+// 	} else if (i % 3 === 0) {
+// 		console.log("Fizz");
+// 	} else if (i % 5 === 0) {
+// 		console.log("Buzz");
+// 	} else {
+// 		console.log(i);
+// 	}
+// }
+
+// Getting to Know You 
+const thom = ["Thom", 1000, "Christchurch"]
+const karolin = ["Karolin", 16, "New York"]
+const kristyn = ["Kristyn", 5, "Pittsburgh"]
+const matt = ["Matt H", 186, "Philadelphia"]
+
+thom[0] = "Gameboy";
+karolin[1] = 17;
+matt[2] = "Gotham City";
+kristyn[2] = "Brooklyn";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
