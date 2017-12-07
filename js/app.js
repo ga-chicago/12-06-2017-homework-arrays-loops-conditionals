c31a417e1e218005d826d3a1cbdce7e5f12531f4
Kait Mikitin <kait.mikitin@gmail.com>
2017-12-06 19:42:28 -0600

    Commit 4 - Fizz Buzz answered

--- js/app.js
+++ js/app.js
@@ -16,12 +16,25 @@
 // 	console.log("Love me, pet me! HSSSSSS!");
 // 	i++;
 // }
-const catThoughts = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."]
+// const catThoughts = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."]
 
-for (i = 1; i <=20; i++) {
-	if(i % 2 === 0) {
-		console.log(catThoughts[Math.floor(Math.random()*3)]);
+// for (i = 1; i <=20; i++) {
+// 	if(i % 2 === 0) {
+// 		console.log(catThoughts[Math.floor(Math.random()*3)]);
+// 	} else {
+// 		console.log("Love me, pet me! HSSSSSS!");
+// 	}
+// }
+
+// Fizz Buzz
+for (i = 1; i <= 100; i++) {
+	if (i % 3 === 0 && i % 5 === 0) {
+		console.log("FizzBuzz");
+	} else if (i % 3 === 0) {
+		console.log("Fizz");
+	} else if (i % 5 === 0) {
+		console.log("Buzz");
 	} else {
-		console.log("Love me, pet me! HSSSSSS!");
+		console.log(i);
 	}
 }
