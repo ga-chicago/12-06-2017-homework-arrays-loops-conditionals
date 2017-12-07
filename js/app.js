b00fbb9af563bb92e5f509d3674705be1e74c4d1
sergio <scrummish@gmail.com>
2017-12-06 21:59:48 -0600

    Fizz Buzz Answered

--- js/app.js
+++ js/app.js
@@ -11,7 +11,6 @@
 // Excited Kitten
 // let catQuotes = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
 // let randomQuote;
-
 // for (let i = 0; i < 20; i++) {
 // 	console.log("Love me, pet me! HSSSSSS!");
 // 	if (i % 2 === 0) {
@@ -19,3 +18,52 @@
 // 		console.log(catQuotes[randomQuote]);
 // 	}
 // };
+
+// Fizz Buzz
+// for (let i = 1; i <= 100; i++) {
+// 	if (!(i % 5) && !(i % 3)) {
+// 		console.log("Fizzbuzz");
+// 	} else if (!(i % 5)) {
+// 		console.log("Buzz");
+// 	} else if (!(i % 3)) {
+// 		console.log("Fizz");
+// 	} else {
+// 		console.log(i);
+// 	}
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
