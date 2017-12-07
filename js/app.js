5d90f44e32818fa4cadb4912e5cd87f2648587e3
Michael Weber <webermn15@gmail.com>
2017-12-06 19:58:37 -0600

    fizz buzz answered

--- js/app.js
+++ js/app.js
@@ -11,17 +11,32 @@
 // }
 
 //Excited Kitten
-for (let i = 0; i <= 20; i++) {
-	console.log("love me, pet me! HSSSS");
-	if ((i % 2) == 0) {
-		let catSays = Math.floor(Math.random()*3)+1;
-		console.log(catSays);
-		if (catSays === 1) {
-			console.log("...human... why are you taking pictures of me");
-		} else if (catSays === 2) {
-			console.log("...the catnip made me do it");
-		} else {
-			console.log("...why does the red dot always get away...");	
-		}
+// for (let i = 0; i <= 20; i++) {
+// 	console.log("love me, pet me! HSSSS");
+// 	if ((i % 2) == 0) {
+// 		let catSays = Math.floor(Math.random()*3)+1;
+// 		console.log(catSays);
+// 		if (catSays === 1) {
+// 			console.log("...human... why are you taking pictures of me");
+// 		} else if (catSays === 2) {
+// 			console.log("...the catnip made me do it");
+// 		} else {
+// 			console.log("...why does the red dot always get away...");	
+// 		}
+// 	}
+// }
+
+//Fizz Buzz
+
+for (let i = 0; i <= 100; i++) {
+	console.log(i);
+	if (i % 5 === 0 && i % 3 === 0) {
+		console.log("fizzbuzz");
+	}
+	else if (i % 3 === 0) {
+		console.log("fizz");
+	}
+	else if (i % 5 === 0) {
+		console.log("buzz");
 	}
 }
