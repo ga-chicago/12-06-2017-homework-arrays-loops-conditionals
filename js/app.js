2aab07d9eac526788cd9232ce2a687f8b5e37afe
sergio <scrummish@gmail.com>
2017-12-06 21:40:40 -0600

    Excited Kittens answer

--- js/app.js
+++ js/app.js
@@ -8,3 +8,14 @@
 // 	console.log(i);
 // };
 
+// Excited Kitten
+// let catQuotes = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
+// let randomQuote;
+
+// for (let i = 0; i < 20; i++) {
+// 	console.log("Love me, pet me! HSSSSSS!");
+// 	if (i % 2 === 0) {
+// 		randomQuote = Math.floor((Math.random() * catQuotes.length-1) + 1);
+// 		console.log(catQuotes[randomQuote]);
+// 	}
+// };
