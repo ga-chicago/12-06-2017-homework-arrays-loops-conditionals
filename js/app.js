15ddf0e2ddb0b58313e0b7bddfef29d20964debc
Charles <because@charlesis.me>
2017-12-07 01:00:12 -0600

    Commit 3 - Excited Kittens answered

--- js/app.js
+++ js/app.js
@@ -6,6 +6,22 @@
 
 //<!-- Get Even -->
 
-for (i = 0; i <=200 ; i+=2) {
-    console.log(i);
-};
+//for (i = 0; i <=200 ; i+=2) {
+//    console.log(i);
+//};
+
+//<!-- Excited Kitten -->
+
+const qittyQuotes = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
+
+const randomQuotes = qittyQuotes[Math.floor(Math.random() * qittyQuotes.length)];
+
+        for (i = 0; i <= 20; i++) {
+
+            if (i % 2 == 0) {
+                console.log(randomQuotes)
+            } else if (i % 2 != 0) {
+                console.log(i);
+            };
+                
+        };
