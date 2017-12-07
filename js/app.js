9f6e4cd4f2c9d16ee50d85d3621e4452f64ec886
Charles <because@charlesis.me>
2017-12-07 09:30:17 -0600

    Commit 8 - I loops through their closets.

--- js/app.js
+++ js/app.js
@@ -55,8 +55,59 @@
 //<!-- Yell at the Ninja Turtles -->
 
 
-var ogs = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"];
+//var ogs = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"];
+//
+//for (ogs.length <= 3; ogs.length++; ) {
+//    console.log(ogs.toUpperCase())
+//};
 
-for (ogs.length <= 3; ogs.length++; ) {
-    console.log(ogs.toUpperCase())
+//<!-- Return of the Closets -->
+
+
+const kristynsCloset = [
+  "left shoe",
+  "cowboy boots",
+  "right sock",
+  "GA hoodie",
+  "green pants",
+  "yellow knit hat",
+  "marshmallow peeps"
+];
+
+// Thom's closet is more complicated. Check out this nested data structure!!
+const thomsCloset = [
+  [
+    // These are Thom's shirts
+    "grey button-up",
+    "dark grey button-up",
+    "light blue button-up",
+    "blue button-up",
+  ],[
+    // These are Thom's pants
+    "grey jeans",
+    "jeans",
+    "PJs"
+  ],[
+    // Thom's accessories
+    "wool mittens",
+    "wool scarf",
+    "raybans"]]
+
+//    
+//var shoe = kristynsCloset.splice(0,1);
+//
+//thomsCloset.push(shoe);
+
+//for (i = 0; i <= kristynsCloset.length; i++) {
+//    console.log("WHIRR: Now washing " + kristynsCloset[i]);
+//}
+
+
+for (i = 0,0,0; i <= thomsCloset.length; i++  ) {
+    console.log(thomsCloset[i])
 };
+
+
+
+    
+    
