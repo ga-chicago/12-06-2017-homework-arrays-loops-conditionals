d37bbc91a1688737686cfe62b6db5247c06ff065
JacksonMcgrath <jacksonfmcgrath@gmail.com>
2017-12-06 20:32:18 -0600

    Commit 3 - Excited Kittens answered

--- js/app.js
+++ js/app.js
@@ -6,6 +6,30 @@
 
 // 2. Get Even
 
-for (let a = 0; a <= 200; a +=2) {
-	console.log(a);
-}
+// for (let a = 0; a <= 200; a +=2) {
+// 	console.log(a);
+// }
+
+// 3. Excited Kitten
+
+// let cat1 = "...human...why you taking pictures of me?..."
+// let cat2 = "...the catnip made me do it..."
+// let cat3 = "...why does the red dot always get away..."
+
+// for (let b = 0; b <= 20; b++) {
+// 	if ((b % 2)== 0) {
+// 		let random1 = (Math.floor(Math.random() * 10 + 1))
+// 		if (random1 < 3) {
+// 			console.log(cat1)
+// 		}
+// 		else if (random1 >= 3 && <= 6){
+// 			console.log(cat2)
+// 		}
+// 		else if (random1 >6){
+// 			console.log(cat3)
+// 		} 
+// 	}
+// 	else {
+// 		console.log("Love me, pet me! HSSSSSS!")
+// 	}
+// }
