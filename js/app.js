a872a67604789012ba5383031df9b7076e206880
Charles <because@charlesis.me>
2017-12-07 01:08:51 -0600

    Commit 4 - Fizz Buzz answered

--- js/app.js
+++ js/app.js
@@ -12,16 +12,25 @@
 
 //<!-- Excited Kitten -->
 
-const qittyQuotes = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
+//const qittyQuotes = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
+//
+//const randomQuotes = qittyQuotes[Math.floor(Math.random() * qittyQuotes.length)];
+//
+//        for (i = 0; i <= 20; i++) {
+//
+//            if (i % 2 == 0) {
+//                console.log(randomQuotes)
+//            } else if (i % 2 != 0) {
+//                console.log(i);
+//            };
+//                
+//        };
 
-const randomQuotes = qittyQuotes[Math.floor(Math.random() * qittyQuotes.length)];
-
-        for (i = 0; i <= 20; i++) {
-
-            if (i % 2 == 0) {
-                console.log(randomQuotes)
-            } else if (i % 2 != 0) {
-                console.log(i);
-            };
-                
-        };
+for (i = 1; i <= 100; i++) {
+    
+    if (i % 3 == 0) {
+        console.log("Fizz");
+    } else if (i % 5 == 0) {
+        console.log("Buzz");
+    } else (console.log ("FizzBuzz"))
+};
