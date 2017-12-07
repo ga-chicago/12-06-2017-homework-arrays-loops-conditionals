32e09bdfc7a19d7dc238d6b9561a6fcde4a69ec7
JacksonMcgrath <jacksonfmcgrath@gmail.com>
2017-12-06 19:38:51 -0600

    Get Even answered

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
-for (let i = 1; i <= 20; i++) {
-	console.log(i);
+// 1. Easy Going
+
+// for (let i = 1; i <= 20; i++) {
+// 	console.log(i);
+// }
+
+// 2. Get Even
+
+for (let a = 0; a <= 200; a +=2) {
+	console.log(a);
 }
