b33111f983bcf582ac537da5f05974f0930faea6
ethan375 <ethanebel1@gmail.com>
2017-12-07 09:05:29 -0600

    Commit 4 - Fizz Buzz answered

--- js/app.js
+++ js/app.js
@@ -6,6 +6,32 @@
 
 // Get Even
 
-for(let x = 0; x <= 200; x++){
-  if((x % 2) === 0) console.log(x);
-}
+// for(let x = 0; x <= 200; x++){
+//   if((x % 2) === 0) console.log(x);
+// }
+
+// Excited Kitten
+
+// let catQuotes = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
+// let randomQuote = undefined;
+
+// for (let i = 0; i < 20; i++) {
+//     console.log("Love me, pet me! HSSSSSS!");
+// };
+
+
+// fizz buzz
+
+// for(let i = 1; i <= 100; i++){
+//   (!(i % 5) && !(i % 3)) {
+//     console.log("fizzbuzz");
+//   } else if (!(i % 5)) {
+//     console.log("buzz");
+//   } else if(!(i % 3)) {
+//     console.log("fizz");
+//   } else{
+//     console.log(i)
+//   }
+
+
+
