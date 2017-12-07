7460d728ff95b1312258175cb116c266f9f681ff
JacksonMcgrath <jacksonfmcgrath@gmail.com>
2017-12-06 20:56:58 -0600

    Commit 4 - FizzBuzz answered

--- js/app.js
+++ js/app.js
@@ -33,3 +33,17 @@
 // 		console.log("Love me, pet me! HSSSSSS!")
 // 	}
 // }
+
+// 4. Fizz Buzz
+
+for (let c = 1; c <= 100; c++){
+	if ((c % 3) == 0){
+		console.log("Fizz")
+	}
+	else if((c % 5) == 0){
+		console.log("Buzz")
+	}
+	else if(((c % 5) == 0) && ((c % 3) == 0)) {
+		console.log("FizzBuzz")
+	}
+}
