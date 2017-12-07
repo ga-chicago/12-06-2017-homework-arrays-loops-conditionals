c5d81c5ae92d540b965d3f7d40284a83cd2aa2a2
Charles <because@charlesis.me>
2017-12-07 00:33:34 -0600

    Commit 2 - Get Even answered

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 //<!-- Easy Going -->
 
-for (i = 1; i <= 20; i++) {
+//for (i = 1; i <= 20; i++) {
+//    console.log(i);
+//};
+
+//<!-- Get Even -->
+
+for (i = 0; i <=200 ; i+=2) {
     console.log(i);
 };
