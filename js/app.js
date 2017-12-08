b7f22f51d0d7501b1c2a500dbbfa9b3ec2eb970d
Anthony <anthonyjlower@gmail.com>
2017-12-07 18:36:31 -0600

    Commit 10 - Triangles answered

--- js/app.js
+++ js/app.js
@@ -73,36 +73,36 @@
 
 	// Return of the closets
 
-const kristynsCloset = [
-  "left shoe",
-  "cowboy boots",
-  "right sock",
-  "GA hoodie",
-  "green pants",
-  "yellow knit hat",
-  "marshmallow peeps"
-];
+// const kristynsCloset = [
+//   "left shoe",
+//   "cowboy boots",
+//   "right sock",
+//   "GA hoodie",
+//   "green pants",
+//   "yellow knit hat",
+//   "marshmallow peeps"
+// ];
 
-// Thom's closet is more complicated. Check out this nested data structure!!
-const thomsCloset = [
-  [
-    // These are Thom's shirts
-    "grey button-up",
-    "dark grey button-up",
-    "light blue button-up",
-    "blue button-up",
-  ],[
-    // These are Thom's pants
-    "grey jeans",
-    "jeans",
-    "PJs"
-  ],[
-    // Thom's accessories
-    "wool mittens",
-    "wool scarf",
-    "raybans"
-  ]
-];
+// // Thom's closet is more complicated. Check out this nested data structure!!
+// const thomsCloset = [
+//   [
+//     // These are Thom's shirts
+//     "grey button-up",
+//     "dark grey button-up",
+//     "light blue button-up",
+//     "blue button-up",
+//   ],[
+//     // These are Thom's pants
+//     "grey jeans",
+//     "jeans",
+//     "PJs"
+//   ],[
+//     // Thom's accessories
+//     "wool mittens",
+//     "wool scarf",
+//     "raybans"
+//   ]
+// ];
 
 
 	// Alien Shoe
@@ -153,13 +153,46 @@ const thomsCloset = [
 
 
 
+  // Triangles
+
+
+  // 1)
+// var argument = 7;
+
+// const triangle = (hash) => {
+//   for(i = 1; i <= argument; i++){
+//     console.log(hash.repeat(i));
+//   };
+// }
+
+
+// 2)
+// var argument = 7;
+
+// const rightIsoTriangle = (space, hash) => {
+//   for(i = 1; i <= argument; i++){
+//     console.log(space.repeat(argument-i) + hash.repeat(i));
+//   };
+// };
 
 
 
+// 3)
+// var argument = 7;
+
+// const triangle = (hash) => {
+//   for(i = argument; i >= 1; i--){
+//     console.log(hash.repeat(i));
+//   };
+// };
 
 
+// 4)
+var argument = 9;
 
-
-
-
+const rightIsoTriangle = (space, hash) => {
+  for(i = argument; i >= 1; i--){
+    console.log(space.repeat(argument-i) + hash.repeat(i));
+  };
+};
 
